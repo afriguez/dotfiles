8acc1983de3254035f12c137495fb9d3d3e3fb2f
Fer L. <afriguez@gmail.com>
2024-01-24 18:47:36 -0500

    feat: update bootstrap & install scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,27 +9,18 @@ ssh_dir="$work_tree/.ssh"
 ssh_key="$work_tree/.ssh/id_ed25519"
 email="afriguez@gmail.com"
 
-sudo pacman -S --needed openssh github-cli --noconfirm
-
-# Create SSH dir if it does not exist
-mkdir -p $ssh_dir
-
-# Generate SSH key
-ssh-keygen -t ed25519 -C $email -f $ssh_key
-
-# Start SSH Agent & add key
-eval "$(ssh-agent -s)"
-ssh-add $ssh_key
-
-# GitHub
-echo "Enter your GitHub API Auth Token"
-echo -n "Token: "
-read -s gh_token
-
-GH_TOKEN=$gh_token $(command -v gh) ssh-key add $ssh_key.pub -t "Linux Ins"
-
-# Clone dotfiles
-git clone $dotfiles_repo --bare $dotfiles_dir
+clone_dotfiles () {
+	git clone $dotfiles_repo --bare $dotfiles_dir > /dev/null 2>&1 &
+	local pid=$!
+	spin='-\|/'
+	i=0
+	while kill -0 $pid 2>/dev/null
+	do
+		i=$(( (i+1) %4 ))
+		printf "\r${spin:$i:1} Cloning dotfiles repository..."
+		sleep .1
+	done
+}
 
 c_git () {
 	$(command -v git) \
@@ -38,11 +29,42 @@ c_git () {
 		"$@"
 }
 
+sudo pacman -S --needed openssh github-cli --noconfirm > /dev/null 2>&1
+
+# Create SSH dir if it does not exist
+mkdir -p $ssh_dir
+
+# Generate SSH key
+echo "Generating SSH key..."
+ssh-keygen -t ed25519 -C $email -f $ssh_key > /dev/null 2>&1
+
+# Start SSH Agent & add key
+eval "$(ssh-agent -s)" > /dev/null 2>&1
+ssh-add $ssh_key > /dev/null 2>&1
+
+# GitHub
+echo "Enter your GitHub API Auth Token"
+echo -n "Token: "
+read -s gh_token
+
+echo -e "\033c"
+GH_TOKEN=$gh_token $(command -v gh) ssh-key add $ssh_key.pub -t "Linux Ins"
+
+# hide cursor
+echo -e "\e[?25l"
+clone_dotfiles
+echo -e "\r\033[32m✓\033[0m Repository cloned successfully."
+
+# show cursor
+echo -e "\e[?25h"
+
 # Restore dotfiles
 c_git restore --staged .
 c_git restore .
 
-echo "Dotfiles bootstrap completed successfully."
+sleep 1
+echo -e "\r\033[32m✓\033[0m Dotfiles bootstrap completed successfully.\n"
 
-echo "Starting Dotfiles installation..."
-source $work_tree/install.sh
+printf "\033[33mRunning Dotfiles Installation...\033[0m"
+$work_tree/install.sh > /dev/null 2>&1
+echo -e "\r\033[32m✓\033[0m Dotfiles installation finished successfully."
--- install.sh
+++ install.sh
@@ -6,6 +6,16 @@ sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort $HOME/pkglist.tx
 
 $(command -v git) --git-dir=$HOME/dotfiles/ --work-tree=$HOME submodule update --init --recursive
 
+cd $HOME
+$(command -v git) clone https://aur.archlinux.org/pikaur.git pikaur
+
+cd $HOME/pikaur
+makepkg -fsri
+
+pikaur -Syu
+
+pikaur -S $(cat $HOME/aurpkglist.txt)
+
 cd $HOME/.config/dwm/
 sudo make clean install
 
@@ -13,5 +23,3 @@ cd $HOME/.config/dmenu/
 sudo make clean install
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-
-echo "Dotfiles installation finished successfully."
